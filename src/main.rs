27e2c3d1b03df5c23cd8fb34832def396d88c996
githvinayak <vinayak192002@gmail.com>
2026-04-22 21:53:42 +0530

    feat: implement add_task with borrowing rules

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,133 @@
+// Day 1-2 - Ownership in Rust
+// struct Task {
+//     id: i32,
+//     title: String,
+//     done: bool,
+// }
 
+// fn main() {
+//     // integers
+//     let task1 = Task {
+//         id: 1,
+//         title: String::from("Learn Rust"),
+//         done: false,
+//     };
+//     let x: i32 = 5;
+//     let y = x;
+//     println!("x:{} y:{}", x, y);
+
+//     // floats
+//     let a: f64 = 3.14;
+//     let b = a;
+//     println!("a:{} b:{}", a, b);
+
+//     // bool
+//     let t: bool = true;
+//     let f = t;
+//     println!("t:{} f:{}", t, f);
+
+//     // char
+//     let c1: char = 'R';
+//     let c2 = c1;
+//     println!("c1:{} c2:{}", c1, c2);
+
+//     // now String
+//     let s1 = String::from("hello");
+//     let _s2 = s1;
+//     // println!("s1:{}", s1); // what happens here?
+//     println!(
+//         "task id:{} and task title:{} and task status:{}",
+//         task1.id, task1.title, task1.done
+//     )
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     #[test]
+//     fn test_task_creation() {
+//         let task = Task {
+//             id: 1,
+//             title: String::from("Learn Rust"),
+//             done: false,
+//         };
+//         assert_eq!(task.id, 1);
+//         assert_eq!(task.title, "Learn Rustt");
+//         assert_eq!(task.done, false);
+//     }
+// }
+
+// Day 3 - Borrowing and References in Rust
+
+#[derive(Debug)]
 struct Task {
-    id: i32,
+    id: u32,
     title: String,
     done: bool,
 }
 
-fn main() {
-    // integers
-    let task1 = Task {
-        id: 1,
-        title: String::from("Learn Rust"),
-        done: false,
+// fn print_task(task:&Task){
+//     println!("id:{} title:{} done:{}", task.id, task.title, task.done);
+// }
+
+// fn mar_down(task:&mut Task){
+//     task.done = true
+// }
+// fn main(){
+//  let mut task1 = Task{
+//     id:1,
+//     title:String::from("Learn Borrowing"),
+//     done:false
+//  };
+//  print_task(&task1);
+//  mar_down(&mut task1);
+//  println!("task title:{}",task1.done);
+//  println!("task1 :{:#?}",task1)
+// }
+
+// fn main() {
+//     let mut task1 = Task {
+//         id: 1,
+//         title: String::from("Learn Rust"),
+//         done: false,
+//     };
+
+//     let r1 = &task1;      // immutable borrow
+//     let r2 = &task1;      // another immutable borrow
+//     let r3 = &mut task1;  // mutable borrow
+
+//     println!("{} {} {}", r1.title, r2.title, r3.title);
+// }
+
+fn add_task(tasks: &mut Vec<Task>, id: u32, title: String, done: bool) {
+    let task = Task {
+        id: id,
+        title: title,
+        done: done,
     };
-    let x: i32 = 5;
-    let y = x;
-    println!("x:{} y:{}", x, y);
+    tasks.push(task)
+}
 
-    // floats
-    let a: f64 = 3.14;
-    let b = a;
-    println!("a:{} b:{}", a, b);
+fn main() {
+    let mut tasks: Vec<Task> = Vec::new();
+    add_task(&mut tasks, 1, String::from("leran rust"), false);
+    add_task(&mut tasks, 1, String::from("leran rust"), false);
+    add_task(&mut tasks, 1, String::from("leran rust"), false);
 
-    // bool
-    let t: bool = true;
-    let f = t;
-    println!("t:{} f:{}", t, f);
+    for task in &tasks {
+        println!("task : {:#?}", task)
+    }
+}
 
-    // char
-    let c1: char = 'R';
-    let c2 = c1;
-    println!("c1:{} c2:{}", c1, c2);
-
-    // now String
-    let s1 = String::from("hello");
-    let s2 = s1;
-    println!("s1:{}", s1); // what happens here?
-    println!(
-        "task id:{} and task title:{} and task status:{}",
-        task1.id, task1.title, task1.done
-    )
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_add_task() {
+        let mut tasks: Vec<Task> = Vec::new();
+        add_task(&mut tasks, 1, String::from("learn rust"), false);
+        assert_eq!(tasks.len(), 1);
+        assert_eq!(tasks[0].id, 1);
+        assert_eq!(tasks[0].title, "learn rust");
+    }
 }
