3364271d05cccba3be7991b050c95e7980958730
githvinayak <vinayak192002@gmail.com>
2026-05-19 13:54:19 +0530

    cli: extract Task struct into task.rs

---
+++ src/task.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Task {
+    pub id: u32,
+    pub title: String,
+    pub done: bool,
+}
+
+impl Task {
+    pub fn new(id: u32, title: String) -> Task {
+        Task {
+            id,
+            title,
+            done: false,
+        }
+    }
+
+    pub fn mark_done(&mut self) {
+        self.done = true;
+    }
+}
+
