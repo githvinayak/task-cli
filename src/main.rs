40cd000c7109588bde4e93a1938e2728dc55b164
githvinayak <vinayak192002@gmail.com>
2026-05-21 17:22:43 +0530

    cli: improve error handling messages

--- src/main.rs
+++ src/main.rs
@@ -214,17 +214,42 @@ const FILE_PATH: &str = "tasks.json";
 fn load_tasks() -> Vec<Task> {
     // 1. check if file exists
     if std::path::Path::new(FILE_PATH).exists() {
-        let content = std::fs::read_to_string(FILE_PATH).unwrap();
-        return serde_json::from_str(&content).unwrap();
+        return Vec::new();
     }
-    return Vec::new();
+     let content = match std::fs::read_to_string(FILE_PATH){
+        Ok(c)=> c,
+        Err(e)=>{
+         println!("cannot read file : file might be corrupted {}",e);
+          return Vec::new();
+        }
+     };
+
+     match serde_json::from_str(&content){
+        Ok(tasks)=> tasks,
+        Err(e)=>{
+         println!("cannot read file : file might be corrupted {}",e);
+          return Vec::new();
+        }
+     }
 }
 
 fn save_tasks(tasks: &[Task]) {
     // 1. convert Vec<Task> to JSON string
-    let content = serde_json::to_string_pretty(tasks).unwrap();
-    // 2. write to tasks.json
-    std::fs::write(FILE_PATH, content).unwrap()
+    // let content = 
+   let content = match serde_json::to_string_pretty(tasks){
+        Ok(c)=> c,
+        Err(e)=>{
+            println!("⚠️ could not serialize tasks: {}", e);
+            return;
+        }
+    };
+    // 2. wlrite to tasks.json
+    match std::fs::write(FILE_PATH, content){
+        Ok(_)=> {},
+        Err(e)=>{
+            println!("⚠️ could not save tasks: {}", e);
+        }
+    };
 }
 fn list_tasks(tasks: &[Task]) {
     for task in tasks {
