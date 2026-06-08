ceb7e93737a8d80ff0458a9a655176bb00d58ae8
githvinayak <vinayak192002@gmail.com>
2026-06-08 17:29:45 +0530

    cleanup: combine derives in Task struct

--- src/task.rs
+++ src/task.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone)]
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Task {
     pub id: u32,
     pub title: String,
